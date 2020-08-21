255ddd1653889bf46d105f9d53e728da8fd00818
MehdiTaher07 <69479765+MehdiTaher07@users.noreply.github.com>
2020-08-21 11:31:44 +0100

    Add files via upload

---
+++ write until correct.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	
+	int n,i;
+	/*do{
+		printf("N:");
+		scanf("%d",&n);	
+	}while(n<1 || n>3);
+	while(n<1 || n>3){
+		printf("incorrect , repeteer N:");
+		scanf("%d",&n);
+	}
+	
+		printf("N:");
+		scanf("%d",&n);	
+		i=1;
+	do{
+		printf("%d\n",n+i);
+		i++;
+	}while(i=<10);
+	
+	printf("Good");
+	*/
+	return 0;
+}
